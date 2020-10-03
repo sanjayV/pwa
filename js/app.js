9b8adb14655bc270f3a5cf814b8e2165eb795395
sanjay <skumarverma45@gmail.com>
2020-10-03 16:24:45 +0530

    trying to fixed service workder path issue

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/sw.js")
+    .register("sw.js")
     .then((reg) => console.log("Service worker registered successfully", reg))
     .catch((err) =>
       console.log("Service worker not registered due to following error", err)
